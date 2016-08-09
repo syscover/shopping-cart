3a7dd4e15cb3a0267ece48838e0a8594c0e820a4
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-08-09 12:20:53 +0200

    minor fix

--- src/config/shoppingcart.php
+++ src/config/shoppingcart.php
@@ -11,10 +11,6 @@ return [
     |
     */
 
-    // Tax default values
-    'taxCountry'                    => env('TAX_COUNTRY', 'ES'),
-    'taxCustomerClass'              => env('TAX_CUSTOMER_CLASS', 1),
-
     // 1 excluding tax, 2 including tax
     'taxProductPrices'              => env('TAX_PRODUCT_PRICES', 1),
     'taxShippingPrices'             => env('TAX_SHIPPING_PRICES', 1),
